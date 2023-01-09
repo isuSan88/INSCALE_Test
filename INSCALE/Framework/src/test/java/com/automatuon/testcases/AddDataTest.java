cef2709a7cb9262656193c952063c08a8fa02c14
isuSan88 <38488861+isuSan88@users.noreply.github.com>
2023-01-09 12:49:50 +0800

    Test

--- INSCALE/Framework/src/test/java/com/automatuon/testcases/AddDataTest.java
+++ INSCALE/Framework/src/test/java/com/automatuon/testcases/AddDataTest.java
@@ -65,7 +65,7 @@ public class AddDataTest extends BaseClass{
 	public void DeleteCustomer()
 	{
 		DeleteCustomerPage deletecustomer= PageFactory.initElements(driver, DeleteCustomerPage.class);
-		deletecustomer.deletecustomer("FirstName", "LastName", "Postcode");
+		deletecustomer.deletecustomer("FirstNamea", "LastName", "Postcode");
 		Helper.captureScreenshot(driver);
 	}
 	
